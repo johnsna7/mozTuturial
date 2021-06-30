bb5ce43a0c0a2af2c8166e7c81bedec4e0b1f1fa
Nathan Johnson <johnsna7@oregonstate.edu>
2021-06-29 21:37:02 -0500

    netlify test

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> near misses
+        <strong>{results.length}</strong> near misses. Oh snap!
       </p>
       <hr></hr>
       {results
